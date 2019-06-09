d894294ec4644f9e9720ee10284a6a3bb2187b76
Tim Golding <cybersaurus@users.noreply.github.com>
2019-06-09 23:17:50 +0100

    Day 01.

---
+++ src/main/scala/cybersaurus/adventofcode/aoc2018/Day01.scala
@@ -0,0 +1,90 @@
+package cybersaurus.adventofcode.aoc2018
+
+import scala.annotation.tailrec
+import scala.io.Source
+
+trait Day01 {
+
+  def firstRepeat(cs: Seq[Int]): Int
+
+  protected final def infiniteStreamOf(cs: Seq[Int]): Stream[Int] =
+    Stream.continually(cs.toStream).flatten
+}
+
+object Day01 {
+  def changes(source: String): Seq[Int] =
+    Source
+      .fromResource(source)
+      .getLines()
+      .map{_.toInt}
+      .toSeq
+
+  def frequency(cs: Seq[Int]): Int = cs.sum
+}
+
+class RecursiveDay01 extends Day01 {
+
+  override def firstRepeat(cs: Seq[Int]): Int = {
+
+    @tailrec
+    def _firstRepeat(remaining: Seq[Int], acc: Seq[Int]): Int = {
+
+      val sum = acc.head + remaining.head
+
+      if (acc.contains(sum)) sum
+      else {
+        _firstRepeat(remaining.tail, sum +: acc)
+      }
+    }
+
+    _firstRepeat(infiniteStreamOf(cs), Seq(0))
+  }
+}
+
+class FoldLeftSlowDay01 extends Day01 {
+
+  override def firstRepeat(cs: Seq[Int]): Int = {
+
+    infiniteStreamOf(cs).foldLeft(Seq(0)) { case (acc, c) =>
+      val sum = acc.last + c
+
+      if (acc.contains(sum)) return sum
+      else acc :+ sum
+    }
+
+    throw new IllegalStateException("Shouldn't get here!")
+  }
+}
+
+class FoldLeftFastDay01 extends Day01 {
+
+  override def firstRepeat(cs: Seq[Int]): Int = {
+
+    infiniteStreamOf(cs).foldLeft((Set(0),0)) { case ((sums,lastSum), c) =>
+      val sum = lastSum + c
+
+      if (sums.contains(sum)) return sum
+      else (sums + sum, sum)
+    }
+
+    throw new IllegalStateException("Shouldn't get here!")
+  }
+}
+
+class ScanLeftDay01 extends Day01 {
+
+  override def firstRepeat(cs: Seq[Int]): Int = {
+
+    val empty = (Set.empty[Int], 0)
+
+    infiniteStreamOf(cs)
+      .scanLeft(empty){ case ((prevSums,lastSum), c) =>
+        val newSum = lastSum + c
+
+        (prevSums + lastSum, newSum)
+      }
+      .find { case (prevSums, lastSum) => prevSums.contains(lastSum) }
+      .map  { case (prevSums, lastSum) => lastSum }
+      .get
+  }
+}
---
+++ src/test/scala/cybersaurus/adventofcode/aoc2018/Day01Spec.scala
@@ -0,0 +1,59 @@
+package cybersaurus.adventofcode.aoc2018
+
+import org.scalatest.{MustMatchers, WordSpec}
+
+
+class Day01Spec extends WordSpec with MustMatchers {
+
+  "Day01" should {
+    val changes = Day01.changes("2018/day01/input.txt")
+
+    "changes" in {
+      changes.size mustBe 959
+    }
+
+    "frequency" in {
+      Day01.frequency(changes) mustBe 445
+    }
+  }
+
+  "RecursiveDay01" should {
+    val changes = Seq(1,2,3,4,-5)
+
+    val testInstance = new RecursiveDay01()
+
+    "firstRepeat" in {
+      testInstance.firstRepeat(changes) mustBe 6
+    }
+  }
+
+  "FoldLeftSlowDay01" should {
+    val changes = Seq(1,2,3,4,-5)
+
+    val testInstance = new FoldLeftSlowDay01()
+
+    "firstRepeat" in {
+      testInstance.firstRepeat(changes) mustBe 6
+    }
+  }
+
+  "FoldLeftFastDay01" should {
+    val changes = Day01.changes("2018/day01/input.txt")
+
+    val testInstance = new FoldLeftFastDay01()
+
+    "firstRepeat" in {
+      testInstance.firstRepeat(changes) mustBe 219
+    }
+  }
+
+  "ScanLeftDay01" should {
+    val changes = Day01.changes("2018/day01/input.txt")
+
+    val testInstance = new ScanLeftDay01()
+
+    "firstRepeat" in {
+      testInstance.firstRepeat(changes) mustBe 219
+    }
+  }
+}
